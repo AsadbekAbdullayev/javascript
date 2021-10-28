de5ac8daddde018b67740ed9ba5234c827920d8c
AsadbekAbdullayev <asadbekmmm127@gmail.com>
2021-10-28 17:13:31 +0300

    test

--- javscript1/index.js
+++ javscript1/index.js
@@ -1,248 +1,63 @@
-// begin 1
-// var a = 10;
-// var p = 4*a;
-// console.log(p);
-
-// begin 2
-// var a = 10;
-// var S = a**2;
-// console.log(S);
-
-// begin 3
-// var a = 10;
-// var b = 5;
-
-// var S = a*b;
-// var P = 2*(a+b);
-// console.log(P);
-// console.log(S);
-
-// begin 4
-// var d = 100;
-// var p = 3.14;
-// var L = p*d;
-// console.log(L);
-
-// begin 5
- // var a = 10;
- // var V = a*a*a;
- // var S = 6*a*a;
- // console.log(V);
- // console.log(S);
-
-// begin 6
-// var a = 1;
-// var b = 2;
-// var c = 3;
-// var S = a*b*c;
-// var P = 2*(a*b + a*c + b*c);
-// console.log(S);
-// console.log(P);
-
-// begin 7
-// var R = 1;
-// var p = 3.14;
-// var S = R*R*p;
-// var L = 2*p*R;
-// console.log(S);
-// console.log(L);
-
-// begin 8	
-// var a = 1;
-// var b = 2;
-// var c = (a+b)/2;
-// console.log(c);
-
-// begin 9
-// var a = 1;
-// var b = 9;
-// var d = a*b;
-// var c = d**0.5;
-// console.log(c);
-
-// begin 10
-// var a =10;
-// var b = 12;
-// var k1 = a**2;
-// var k2 = b**2;
-// var y = (a + b);
-// var k = a*b;
-// console.log(k1);
-// console.log(k2);
-// console.log(y);
-// console.log(k);
-
-// begin 11
-// var a =10;
-// var b = 12;
-// var k1 = a**2;
-// var k2 = b**2;
-// var y = (a + b);
-// var k = a*b;
-// console.log(k1);
-// console.log(k2);
-// console.log(y);
-// console.log(k);
-
-// begin 12
-// var a=9;
-// var b=9;
-// var c=9;
-// var p=a+b+c;
-// console.log(p);
-// var d = (a**2 + b**2);
-// console.log(d**0.5);
-
-// begin 13
-// var r1 = 25;
-// var r2 = 15;
-// var p = 3.14;
-// var S1 = p*r1*r1;
-// var S2 = p*r2*r2;
-// var S3 = S1-S2;
-// console.log(S1);
-// console.log(S2);
-// console.log(S3);
-
-// begin 14
-// l=2pr s=pr2
-// var l = 10;
-// var p = 3.14;
-// var r = l/(2*p);
-// var s = p*r*r;
-// console.log(r);
-// console.log(s);
-
-// begin 16
-// var x1 = 9;
-// var x2 = 19;
-// var x3 = (x1 - x2);
-// var x4 = x3**2;
-// var x5 = x4**0.5;
-// console.log(x5);
-
-// begin 17
-// var A = 10;
-// var B = 20;
-// var C = 30;
-// var AC = C - A;
-// var BC = C - B;
-// var AB = B - A;
-// console.log(AC);
-// console.log(BC);
-// console.log(AB);
-// console.log(AC + BC + AB);
-
-// begin 18
-// var A = 10;
-// var C = 20;
-// var B = 30;
-// var AC = C - A;
-// var BC = B - C;
-// console.log(AC * BC);
-
-// begin 19
-// var Ax = 10;
-// var Ay = 10;
-// var Bx = 10;
-// var By = 12;
-// var Cx = 12;
-// var Cy = 12;
-// var Dx = 12;
-// var Dy = 10;
-// var AB = (By - Ay);
-// var AD = (Dx - Ax);
-// var S = AB*AD;
-// var P = 2*(AB+AD);
-// console.log(S);
-// console.log(P);
-
-// begin 20
-// var x1 = 1;
-// var x2 = 2;
-// var y1 = 3;
-// var y2 = 4;
-// var x3 = (x2-x1);
-// var y3 = (y2-y1);
-// var x = x3**2;
-// var y = y3**2;
-// var z = (x + y);
-// console.log(z**0.5);
-
-// begin 21
-// var x1 = 1;
-// var x2 = 2;
-// var x3 = 5;
-// var y1 = 3;
-// var y2 = 4;
-// var y3 = 5;
-// var x11 = (x2-x1);
-// var y11 = (y2-y1);
-// var x21 = (x3-x1);
-// var y21 = (y3-y1);
-// var x31 = (x3-x2);
-// var y31 = (y3-y2);
-// var z1 = ((y11**2) + (x11**2));
-// var z2 = ((y21**2) + (x21**2));
-// var z3 = ((y31**2) + (x31**2));
-// var a = (z1**0.5);
-// var b = (z2**0.5);
-// var c = (z3**0.5);
-// var p = ((a+b+c)/2);
-// var S = ((p*(p-a)*(p-b)*(p-c))**0.5);
-// console.log(S);
-
-// begin 22
-// var a = 10;
-// var b = 12;
-// var c = a;
-// a = b;
-// b = c;
-//   console.log( a);                                                          
-//  console.log( b);
-// bugun 24-oktabr
-// bugun 25-oktabr
-// bugun 26-oktabr
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+// round pow sqrt cbrt abc ceil floor min max 
+
+// function harf(soz, harf){
+//   let a = `${soz}`;
+// let b = a.repeat(5).toLowerCase().split("").sort();
+// let c1 = b.lastIndexOf(`${harf}`);
+// let c2 = b.indexOf(`${harf}`);
+// let c3 = ((c1-c2)+1);
+// console.log(c3);}
+// harf("Welcome to webbrain academi","w");
+
+// }
+// let Newpow = (a,b)=>{
+//  c = (a**b);
+//  console.log(c);
+// }
+// Newpow(2,5);
+
+// let NewSqrt = (a)=>{
+//  c = (a**0.5);
+//  console.log(c);
+// }
+// NewSqrt(9);
+
+// let NewCbrt = (a)=>{
+//  c = (a**(1/3));
+//  console.log(c);
+// }
+// NewCbrt(8);
+
+
+
+
+// let NewABC = (a)=>{
+// 	b = (a**2);
+//  c = (b**(1/2));
+//  console.log(c);
+// }
+// NewABC(-2);
+
+// let Newfloor = (a)=>{
+// 	b = (a*10);
+//  c = (b%10);
+//  g = (b-c);
+//  x = (g/10);
+
+
+//  console.log(x);
+// }
+// Newfloor(1231.7);
+
+// let Newceil = (a)=>{
+// 	b = (a*10);
+//  c = (b%10);
+//  g = (b-c);
+//  x = (g/10);
+//  y = (x+1);
+
+
+//  console.log(y);
+// }
+// Newceil(1231.7);
+// 28-oktabrdddddd
