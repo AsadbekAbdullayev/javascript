6f2f8e4286669f39d5bd669478b2edf9a4b36bb5
AsadbekAbdullayev <asadbekmmm127@gmail.com>
2021-11-07 19:21:46 +0300

    7-noyabr

--- javscript1/index5.js
+++ javscript1/index5.js
@@ -1,11 +1,62 @@
-
-
-    let obj = {
-        id : 37,
+let obj = [
+    {
+        id : 1,
         name : 'Asadbek',
-        surname : 'Abdullayev',
         status : 'talaba',
         grant : true
 
+       
+    },
+    {
+        id : 2,
+        name : 'Olimjon',
+        status : 'talaba',
+        grant : true
+    },
+    {
+        id : 3,
+        name : 'Xolmirza',
+        status : 'talaba',
+        grant : false
+    },
+    {
+        id : 4,
+        name : 'Azamat',
+        status : 'talaba',
+        grant : false
+    },
+    {
+        id : 5,
+        name : 'Xasan',
+        status : 'talaba',
+        grant : false
     }
-    console.log(obj.grant)
+]
+
+    // let OnDelete =(ID)=>{
+        
+    //     for(i=0;i<obj.length;i++){
+    //         if(obj[i].id === ID){
+    //             obj .splice(i,1)
+    //         }
+    //     }
+    //     console.log(obj)
+    // }
+   
+    // Ondelete(2);
+    
+
+// let OnSort= (Value)=>{
+
+// let V = `${Value}`;
+// let Newobj = []
+//         for(i=0;i<obj.length;i++){
+//             if(obj[i].name === V){
+//                  Newobj.push(obj[i])
+//             }
+//         }
+//         console.log(Newobj)
+//     }
+// OnSort('Asadbek')
+
+    
