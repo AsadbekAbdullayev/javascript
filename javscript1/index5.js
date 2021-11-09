a527e0746a33954798ff2cda6afeeb2e900becc7
AsadbekAbdullayev <asadbekmmm127@gmail.com>
2021-11-09 19:38:38 +0300

    9-noyabr

--- javscript1/index5.js
+++ javscript1/index5.js
@@ -1,37 +1,37 @@
-let obj = [
-    {
-        id : 1,
-        name : 'Asadbek',
-        status : 'talaba',
-        grant : true
+// let obj = [
+//     {
+//         id : 1,
+//         name : 'Asadbek',
+//         status : 'talaba',
+//         grant : true
 
        
-    },
-    {
-        id : 2,
-        name : 'Olimjon',
-        status : 'talaba',
-        grant : true
-    },
-    {
-        id : 3,
-        name : 'Xolmirza',
-        status : 'talaba',
-        grant : false
-    },
-    {
-        id : 4,
-        name : 'Azamat',
-        status : 'talaba',
-        grant : false
-    },
-    {
-        id : 5,
-        name : 'Xasan',
-        status : 'talaba',
-        grant : false
-    }
-]
+//     },
+//     {
+//         id : 2,
+//         name : 'Olimjon',
+//         status : 'talaba',
+//         grant : true
+//     },
+//     {
+//         id : 3,
+//         name : 'Xolmirza',
+//         status : 'talaba',
+//         grant : false
+//     },
+//     {
+//         id : 4,
+//         name : 'Azamat',
+//         status : 'talaba',
+//         grant : false
+//     },
+//     {
+//         id : 5,
+//         name : 'Xasan',
+//         status : 'talaba',
+//         grant : false
+//     }
+// ]
 
     // let OnDelete =(ID)=>{
         
@@ -58,6 +58,100 @@ let obj = [
 //         console.log(Newobj)
 //     }
 // OnSort('Asadbek')
+
+// let data1 = {
+//     a:1,
+//     b:1
+// };
+
+// let data2 = {
+//     a:1,
+//     b:1
+// };
+
+
+// let data3 = {
+//     a:1,
+//     b:2
+// };
+
+// let checkSame = (x,y)=>{
+
+// let a = Object.keys(x);
+// let a1 = Object.keys(y);
+// let b = Object.values(x);
+// let b1 = Object.values(y);
+// let aa =  a.toString();
+// let aa1 =  a1.toString();
+// let bb =  b.toString();
+// let bb1 =  b1.toString();
+
+// if((aa === aa1) || (bb === bb1)){
+//     console.log('True')
+
+// }else{
+// console.log('False')
+
+// }
+// }
+// checkSame(data2,data3);
+
+
+
+// let data = {
+//     a:{
+//         s:null,
+//     }
+   
+// } 
+// let nestedValueExistsx = ()=>{
+
+// }
+// let a = Object.values(data);
+// let aa = a[0];
+// let aaa = Object.values(aa);
+// let s = aaa.toString();
+// if(s.length=== 0){
+//     console.log('False')
+// }else{
+//     console.log('True')
+
+// }
+// nestedValueExistsx(data)
+
+
+// let a = Object.keys(data1)
+// let s = Object.values(data2)
+// let d = Object.entries(data3)
+// console.log(a,'keylari ')
+// console.log(s,'valueslari ')
+// console.log(d,'entreis kurinishi ')
+
+
+
+
+// let areEqual = (x,y)=>{
+
+// let a = Object.keys(x);
+// let a1 = Object.keys(y);
+// let b = Object.values(x);
+// let b1 = Object.values(y);
+// let aa =  a.toString();
+// let aa1 =  a1.toString();
+// let bb =  b.toString();
+// let bb1 =  b1.toString();
+
+// if((aa === aa1) && (bb === bb1)){
+//     console.log('True')
+
+// }else{
+// console.log('False')
+
+// }
+// }
+// areEqual(data1,data3);
+
+
 
 
     
