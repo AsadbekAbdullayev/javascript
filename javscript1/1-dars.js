9d47f156c1cbba57e9e4df4308bbc1a65124f6b5
AsadbekAbdullayev <asadbekmmm127@gmail.com>
2021-11-19 12:41:38 +0300

    19-noyabr

--- javscript1/1-dars.js
+++ javscript1/1-dars.js
@@ -1,1 +1,55 @@
+// // ARRAY
 
+// let a = 'Asadbek';
+
+// let b = 21;
+
+// let array =['Asadbek',21];
+
+// // let array =['Nexia', 'Cobalt', 'Spark'];
+
+// let fruits = ["Banana", "Orange", "Apple", "Mango"];
+
+// let f = fruits.join('');
+
+// let f = fruits.pop();  
+
+// let f = fruits.push("Kiwi");
+
+// let f = fruits.shift();
+
+// let f = fruits.unshift("Lemon");
+
+// let f = fruits.splice(2, 0);
+// // OBJECT
+
+// let person = {firstName:"John", lastName:"Doe", age:50, eyeColor:"blue"};
+
+
+// // let a = Object.keys()
+// // let a = Object.values()
+// // let a = Object.entries()
+
+
+// CRUD CREATE READ UPDATE DELETE
+
+// let user = [
+//     {   
+//         id : 2,
+//         name : 'Eshmat',
+//         status : 'CS',
+//         department : 'IT'
+//     },
+//     {
+//         id : 1,
+//         name : 'Gishmat',
+//         status : 'CS',
+//         department : 'AI'
+//     },
+//     {
+//         id : 3,
+//         name : 'Ahmad',
+//         status : 'CS',
+//         department : 'IT'
+//     }
+// ]
--- javscript1/index7.js
+++ javscript1/index7.js
@@ -100,14 +100,26 @@
 // let noRepaet = ()=>{
 //     let b ='';
 //     let c =[];
+//     let sort = a.split('').sort();
 //     for(i=0;i<a.length;i++){
 //         if(b.search(a[i]) === -1){     
 //         c.push(a[i]) 
-        
 //         }
 //         b=c.join('')
 //     }
-// console.log(b);
+// let bb = b.split('');
+// let w =0;
+// for(j=0;j<bb.length;j++){
+//     for(q=0;q<sort.length;q++){
+//         if(bb[j]===sort[q]){
+//             w++;
+//         }
+        
+//     }
+    
+//     console.log(w,'ta',bb[j])
+//     w=0;
+// }
 // }
 // noRepaet();
 
